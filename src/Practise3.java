1ea47bb33788062151e01ad30b8f7d150a7a6960
ananyanayak <ananya.3sept@gmail.com>
2024-03-28 13:14:55 +0530

    practise3 updated

--- src/Practise3.java
+++ src/Practise3.java
@@ -1,16 +1,16 @@
+import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class Practise3 {
 
 	public static void main(String[] args) {
-		
-		
-		
+	
 		WebDriver driver = new ChromeDriver();
 		test1(driver);
 		driver.quit();
@@ -26,9 +26,25 @@ public class Practise3 {
 	{
 
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
-		List<WebElement> list = driver.findElements(By.xpath("//tbody/tr/td[1]"));
-		List<String> list1 = list.stream().filter(s->s.getText().contains("Rice")).map(s->getprice(s)).collect(Collectors.toList());
-		list1.forEach(a->System.out.println(a));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		int count=0;
+		List<WebElement> list;
+		List<String> list1;
+		driver.findElement(By.xpath("//th[contains(@aria-label,'Veg/fruit name')]")).click();
+		do {
+		list = driver.findElements(By.xpath("//tbody/tr/td[1]"));
+		list1 = list.stream().filter(s->s.getText().contains("Rice")).map(s->getprice(s)).collect(Collectors.toList());  //test data rice
+		count = list1.size();
+		
+		//clicking next
+		if(count<1) {
+		driver.findElement(By.xpath("//a[@aria-label='Next']")).click();}
+		}while(count<1);
+	
+		
+		Assert.assertTrue(list1.contains("37"));   //price of rice is 37
+		
+	//	list1.forEach(a->System.out.println(a));
 		driver.quit();
 	}
 
--- src/calendar.java
+++ src/calendar.java
@@ -19,21 +19,16 @@ public class calendar {
 		String month = "4";
 		String year = "2027";
 	calendarsimple(driver, day, month, year);
-		
-		// calendarproper(driver, day, month, year);
-		
-	int date = Integer.parseInt(month);  // converted month from string to integer
-	String month1 = Month.of(date).name();  //uppercase month converted to string for number
-		//year selection
+	
 		assertion(driver, day, month, year);
 		driver.quit();
 	}
 	
-	//method 1
+	
 	public static void calendarsimple(WebDriver driver, String day, String month, String year)
 	{
-		int date = Integer.parseInt(month);  // converted month from string to integer
-		String month1 = Month.of(date).name();  //uppercase month converted to string for number
+		int date = Integer.parseInt(month);  // converted monthnumber from string to integer
+		String month1 = Month.of(date).name();  //uppercase monthname converted to string from number
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		driver.findElement(By.xpath("//div[@class='react-date-picker__wrapper']/button[2]")).click();
@@ -76,88 +71,6 @@ public class calendar {
 	
 	}
 	
-	/* public static void calendarproper(WebDriver driver, String day, String month, String year)
-	{
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 	
-		driver.findElement(By.xpath("//div[@class='react-date-picker__wrapper']/button[2]")).click();
-		driver.findElement(By.cssSelector(".react-calendar__navigation__label")).click();
-		String getyear= driver.findElement(By.xpath("//button[@class='react-calendar__navigation__label']/span")).getText();
-		
-		if(!getyear.equalsIgnoreCase(year))
-		{
-		driver.findElement(By.cssSelector(".react-calendar__navigation__label")).click();
-		List<WebElement> allyears = driver.findElements(By.xpath("//button[contains(@class,'react-calendar__decade-view__years__year')]"));
-		for(int i=0;i<allyears.size();i++)
-		{
-			String syear = allyears.get(i).getText();
-			if(syear.equalsIgnoreCase(year))
-			{
-				allyears.get(i).click();
-			}
-		}
-		List<WebElement> months = driver.findElements(By.xpath("//button[contains(@class,'react-calendar__year-view__months__month')]/abbr"));
-		int date = Integer.parseInt(month); 
-		String month1 = Month.of(date).name(); 
-		for(int j=0;j<months.size();j++)
-		{
-			if(months.get(j).getText().equalsIgnoreCase(month1))
-			{
-				months.get(j).click();
-			}
-		}
-		
-		List<WebElement> days = driver.findElements(By.xpath("//button[contains(@class,'react-calendar__month-view__days__day')]/abbr"));
-		
-
-		for(int i=0;i<days.size();i++)
-			
-		{
-			System.out.println(days.get(i).getText());
-			String[] m = days.get(i).getAttribute("aria-label").split(" ");  //extracting the month from website
-			
-			//using if clause because on some months date day number can occur twice due to previous month date or next month date
-			
-			if(days.get(i).getText().equalsIgnoreCase(day) & m[1].equalsIgnoreCase(month1))
-			{
-				days.get(i).click();
-			}
-		}
-		
-		}
-		
-		else
-		{
-			List<WebElement> months = driver.findElements(By.xpath("//button[contains(@class,'react-calendar__year-view__months__month')]/abbr"));
-			int date = Integer.parseInt(month); 
-			String month1 = Month.of(date).name(); 
-			for(int j=0;j<months.size();j++)
-			{
-				if(months.get(j).getText().equalsIgnoreCase(month1))
-				{
-					months.get(j).click();
-				}
-			}
-			
-			List<WebElement> days = driver.findElements(By.xpath("//button[contains(@class,'react-calendar__month-view__days__day')]/abbr"));
-			
-
-			for(int i=0;i<days.size();i++)
-				
-			{
-				System.out.println(days.get(i).getText());
-				String[] m = days.get(i).getAttribute("aria-label").split(" ");  //extracting the month from website
-				
-				//using if clause because on some months date day number can occur twice due to previous month date or next month date
-				
-				if(days.get(i).getText().equalsIgnoreCase(day) & m[1].equalsIgnoreCase(month1))
-				{
-					days.get(i).click();
-				}
-			}
-			
-		}
-		
-	} */
 
 }
